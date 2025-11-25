4627f3e0b782382549c6b8a269f62fa75865e8be
Nerd <studytime023@gmail.com>
2025-11-25 21:43:35 +0300

    refactor (Application) : automate core tasks

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 echo "Building project packages..."
 pip install -r requirements.txt
 
+echo "Add Database URL..."
+python database_setup.py
+
 echo "Migrating Database..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
