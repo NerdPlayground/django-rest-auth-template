ff9116e59e8f2fb2dda320209f2773dca77ea5f2
Nerd <studytime023@gmail.com>
2025-11-27 11:28:43 +0300

    refactor (Application) : add clean up

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "Add Database URL..."
+python env_setup.py.py
+
 echo "Building project packages..."
 pip install -r requirements.txt
-
-echo "Add Database URL..."
-python database_setup.py
+pip freeze > requirements.txt
 
 echo "Migrating Database..."
 python manage.py makemigrations --noinput
@@ -12,3 +13,6 @@ python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Remove setup files..."
+rm -f env_setup.py.py README.md build.sh
